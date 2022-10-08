4c333c34f53164f325952b26c6a45e171cf82c2f
Amal-benhenia <ambenhenia@gmail.com>
2022-10-08 18:04:13 +0200

    logo second changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,8 +36,8 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand className="logo">
-            <img src={logo} alt="Logo" />
+          <Navbar.Brand >
+            <img src={logo} alt="Logo" style={{"height": "80px"}}/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
